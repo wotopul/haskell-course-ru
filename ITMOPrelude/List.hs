9fba5b652143bfa53c67803a566b1e285ea3fb07
wotopul <wotopul@gmail.com>
2014-11-10 05:27:47 +0300

    folds and scans

--- ITMOPrelude/List.hs
+++ ITMOPrelude/List.hs
@@ -36,7 +36,7 @@ tail (Cons _ xs) = xs
 -- Список без последнего элемента
 init :: List a -> List a
 init Nil = error "init: empty list"
-init (Cons x Nil) = Nil
+init (Cons _ Nil) = Nil
 init (Cons x xs) = Cons x (init xs)
 
 -- Первый элемент
@@ -64,14 +64,14 @@ drop (Succ n) (Cons x xs) = drop n xs
 
 -- Оставить в списке только элементы удовлетворяющие p
 filter :: (a -> Bool) -> List a -> List a
-filter _ Nil = Nil
+filter p Nil = Nil
 filter p (Cons x xs) = if' (p x) (Cons x tail) tail where
     tail = filter p xs
 
 -- Обобщённая версия. Вместо "выбросить/оставить" p
 -- говорит "выбросить/оставить b".
 gfilter :: (a -> Maybe b) -> List a -> List b
-gfilter _ Nil = Nil
+gfilter p Nil = Nil
 gfilter p (Cons x xs) = case p x of Nothing -> tail
                                     Just y  -> Cons y tail
                         where tail = gfilter p xs
@@ -79,7 +79,7 @@ gfilter p (Cons x xs) = case p x of Nothing -> tail
 -- Копировать из списка в результат до первого нарушения предиката
 -- takeWhile (< 3) [1,2,3,4,1,2,3,4] == [1,2]
 takeWhile :: (a -> Bool) -> List a -> List a
-takeWhile _ Nil = Nil
+takeWhile p Nil = Nil
 takeWhile p (Cons x xs) = case p x of True  -> Cons x $ takeWhile p xs
                                       False -> Nil
 
@@ -87,14 +87,14 @@ takeWhile p (Cons x xs) = case p x of True  -> Cons x $ takeWhile p xs
 -- после чего скопировать все элементы, включая первый нарушивший
 -- dropWhile (< 3) [1,2,3,4,1,2,3,4] == [3,4,1,2,3,4]
 dropWhile :: (a -> Bool) -> List a -> List a
-dropWhile _ Nil = Nil
+dropWhile p Nil = Nil
 dropWhile p l@(Cons x xs) = case p x of True  -> dropWhile p xs
                                         False -> l
 
 -- Разбить список по предикату на (takeWhile p xs, dropWhile p xs),
 -- но эффективнее
 span :: (a -> Bool) -> List a -> Pair (List a) (List a)
-span _ Nil = Pair Nil Nil
+span p Nil = Pair Nil Nil
 span p l@(Cons x xs) = case p x of True  -> Pair (Cons x f) s
                                    False -> Pair Nil l
                        where Pair f s = span p xs
@@ -107,8 +107,8 @@ break p = span (not . p)
 -- n-ый элемент списка (считая с нуля)
 (!!) :: List a -> Nat -> a
 Nil !! n = error "!!: empty list"
-(Cons x _) !! Zero = x
-(Cons _ xs) !! (Succ n) = xs !! n
+(Cons x xs) !! Zero = x
+(Cons x xs) !! (Succ n) = xs !! n
 
 -- Список задом на перёд
 reverse :: List a -> List a
@@ -146,12 +146,14 @@ repeat x = Cons x (repeat x)
 --  / \
 -- z  l!!0
 foldl :: (a -> b -> a) -> a -> List b -> a
-foldl f z l = undefined
+foldl f z Nil = z
+foldl f z (Cons x xs) = foldl f (f z x) xs
 
 -- Тот же foldl, но в списке оказываются все промежуточные результаты
 -- last (scanl f z xs) == foldl f z xs
 scanl :: (a -> b -> a) -> a -> List b -> List a
-scanl = undefined
+scanl f z Nil = Cons z Nil
+scanl f z (Cons x xs) = Cons z $ scanl f (f z x) xs
 
 -- Правая свёртка
 -- порождает такое дерево вычислений:
@@ -166,12 +168,14 @@ scanl = undefined
 --            z
 --            
 foldr :: (a -> b -> b) -> b -> List a -> b
-foldr f z l = undefined
+foldr f z Nil = z
+foldr f z (Cons x xs) = f x (foldr f z xs)
 
 -- Аналогично
 --  head (scanr f z xs) == foldr f z xs.
 scanr :: (a -> b -> b) -> b -> List a -> List b
-scanr = undefined
+scanr f z Nil = Cons z Nil
+scanr f z (Cons x xs) = Cons (f x $ head l) l where l = scanr f z xs
 
 -- Должно завершаться за конечное время
 finiteTimeTest = take (Succ $ Succ $ Succ $ Succ Zero) $ foldr (Cons) Nil $ repeat Zero
@@ -188,7 +192,7 @@ concat (Cons x xs) = x ++ concat xs
 
 -- Эквивалент (concat . map), но эффективнее
 concatMap :: (a -> List b) -> List a -> List b
-concatMap _ Nil = Nil
+concatMap f Nil = Nil
 concatMap f (Cons x xs) = f x ++ concatMap f xs
 
 -- Сплющить два списка в список пар длинны min (length a, length b)
@@ -199,6 +203,6 @@ zip (Cons x xs) (Cons y ys) = Cons (Pair x y) (zip xs ys)
 
 -- Аналогично, но плющить при помощи функции, а не конструктором Pair
 zipWith :: (a -> b -> c) -> List a -> List b -> List c
-zipWith _ Nil _ = Nil
-zipWith _ _ Nil = Nil
+zipWith f Nil _ = Nil
+zipWith f _ Nil = Nil
 zipWith f (Cons x xs) (Cons y ys) = Cons (f x y) (zipWith f xs ys)
