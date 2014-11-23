d774612f2c9680e8b6ef94fd92422f0e716e5d72
wotopul <wotopul@gmail.com>
2014-11-23 12:29:34 +0300

    tree, according to http://oxij.org/activity/itmo/fp/2012/

--- ITMOPrelude/Tree.hs
+++ ITMOPrelude/Tree.hs
@@ -1,20 +1,41 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module ITMOPrelude.Tree where
 
-import Prelude (Show, Read, Eq, Ord, (==), (<), (>))
-import ITMOPrelude.Primitive
+import Prelude (Show, Read)
+
+import ITMOPrelude.Algebra
 
 -- Всё что угодно, главное, чтобы соответствовало
 -- заданию
 
-data Tree a = Leaf | Node a (Tree a) (Tree a) deriving (Show, Read, Eq, Ord)
+data Tree a = Leaf | Node a (Tree a) (Tree a) deriving (Show, Read)
+
+undefined = undefined
+
+empty :: Tree a
+empty = Leaf
 
 singleton :: a -> Tree a
-singleton x = Node x Leaf Leaf
+singleton a = Node a Leaf Leaf
 
-elem :: (Ord a) => a -> Tree a -> Bool
-elem _ Leaf = False
-elem x (Node a left right)
-    | x == a = True
-    | x < a  = elem x left
-    | x > a  = elem x right
+insertLeft :: a -> Tree a -> Tree a
+insertLeft a Leaf = Node a Leaf Leaf
+insertLeft a (Node x l r) = Node x (insertLeft a l) r
+
+insertRight :: a -> Tree a -> Tree a
+insertRight a Leaf = Node a Leaf Leaf
+insertRight a (Node x l r) = Node x l (insertRight a r)
+
+rotateLeft :: Tree a -> Tree a
+rotateLeft (Node p a (Node q b c)) = Node q (Node p a b) c
+
+rotateRight :: Tree a -> Tree a
+rotateRight (Node q (Node p a b) c) = Node p a (Node q b c)
+
+treeMap :: (a -> b) -> Tree a -> Tree b
+treeMap f Leaf = Leaf
+treeMap f (Node x l r) = Node (f x) (treeMap f l) (treeMap f r)
+
+foldMap :: (Monoid m) => (a -> m) -> Tree a -> m
+foldMap = undefined
+
