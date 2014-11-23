77d8a8d24687f071729d5cb2f9582d326976d257
wotopul <wotopul@gmail.com>
2014-11-23 11:36:17 +0300

    from bind to fish and join

--- ITMOPrelude/Categories/FromMonad.hs
+++ ITMOPrelude/Categories/FromMonad.hs
@@ -7,3 +7,14 @@ import ITMOPrelude.Categories.MonadJoin
 import ITMOPrelude.Categories.MonadFish
 
 -- делаем нас
+
+instance Monad m => Functor m where
+    fmap f x = x >>= return . f
+
+instance Monad m => MonadJoin m where
+    retutnJoin = return
+    join x = (x >>= id)
+
+instance Monad m => MonadFish m where
+    returnFish = return
+    f >=> f = \x -> (f x >>= g)
