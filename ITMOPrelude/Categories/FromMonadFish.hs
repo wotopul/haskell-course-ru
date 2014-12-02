8c55a6457079dc3f592a26301b00528edddea593
wotopul <wotopul@gmail.com>
2014-12-03 01:08:36 +0300

    small bugfix

--- ITMOPrelude/Categories/FromMonadFish.hs
+++ ITMOPrelude/Categories/FromMonadFish.hs
@@ -2,17 +2,19 @@
 module ITMOPrelude.Categories.ToMonadFish where
 import ITMOPrelude.Categories.MonadFish
 
+import ITMOPrelude.Primitive ((.), id)
+
 -- Эти
-import ITMOPrelude.Categories
+import ITMOPrelude.Categories hiding ((.), id)
 import ITMOPrelude.Categories.MonadJoin
 
 -- делаем из нас
-instance MonadFish m => Monad where
+instance MonadFish m => Monad m where
     return = returnFish
     f >>= g = (id >=> g) f
 
 instance MonadFish m => Functor m where
-    fmap f x = id >=> (return . f)
+    fmap f = id >=> (return . f)
 
 instance MonadFish m => MonadJoin m where
     returnJoin = returnFish
